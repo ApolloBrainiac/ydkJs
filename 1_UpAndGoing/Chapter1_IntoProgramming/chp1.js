def3c02697a3eb963ddd2ad9101b008b395fac30
Andrew M. Perez <AxMPerez@gmail.com>
2017-08-23 00:38:48 -0400

    finish Chp 1 lessons up to 'Blocks'

--- 1_UpAndGoing/Chapter1_IntoProgramming/chp1.js
+++ 1_UpAndGoing/Chapter1_IntoProgramming/chp1.js
@@ -23,3 +23,95 @@ console.log(age);
 // Operators
 // ==============
 
+/*
+// a = 2;
+// b = a + 1;
+
+var a = 20;
+
+a = a + 1;
+a = a * 2;
+
+console.log(a); */
+
+// ==============
+// Values & Types
+// ==============
+
+/*
+"I am a string";
+'I am also a string';
+
+42;
+
+true;
+false;
+*/
+
+// ==============
+// Converting Between Types
+// ==============
+
+/*)
+var a = '42';
+var b = Number(a);
+
+console.log(a);
+console.log(b);
+*/
+
+// ==============
+// Code Comments
+// ==============
+
+/*
+- Code without comments is suboptimal.
+- Too many comments (one per line, for example)
+is probably a sign of poorly written code.
+- Comments should explain why, not what. They can
+optionally explain how if that's particularly confusing.
+*/
+
+//var a = 42;   // 42 is the meaning of life
+
+/* The following value is used because
+   it has been shown that it answers
+   every question in the universe. */
+// var a = 42;
+
+// var a = /* arbitrary value*/ 42;
+// console.log(a); // 42
+
+// ==============
+// Variable
+// ==============
+
+/*
+var amount = 99.99;
+amount = amount * 2;
+
+console.log(amount); //199.98
+
+// convert 'amount' to a string, and
+// add "$" on the beginning
+amount = "$" + String(amount);
+
+console.log (amount); //'$199.98'
+
+
+const TAX_RATE = 0.08; // 8% sales tax
+
+var amount = 99.99;
+
+amount *= 2;
+
+amount += (amount * TAX_RATE);
+
+console.log(amount);						// 215.9784
+console.log(amount.toFixed(2));	// "215.98"
+
+*/
+
+// ==============
+// Blocks
+// ==============
