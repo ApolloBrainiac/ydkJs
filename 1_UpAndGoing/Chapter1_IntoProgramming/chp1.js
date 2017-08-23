ed5ed48a02266c8b94126874155cd58f69573c7c
Andrew M. Perez <AxMPerez@gmail.com>
2017-08-22 23:52:57 -0400

    begin chapter 1

---
+++ 1_UpAndGoing/Chapter1_IntoProgramming/chp1.js
@@ -0,0 +1,25 @@
+// ==============
+// Output
+// ==============
+
+/* a = 21;
+
+b = a * 2;
+
+console.log(b);
+*/
+
+// ==============
+// Input
+// ==============
+
+/* age = prompt('Please tell me your age:');
+
+console.log(age);
+
+*/
+
+// ==============
+// Operators
+// ==============
+
